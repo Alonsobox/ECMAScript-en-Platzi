5a79b5421ff0224618e78b58df07a74709ecfd10
alonsobox <alonsobox_14@hotmail.com>
2023-04-12 16:05:57 -0500

    Agregando EcmaScript10

---
+++ es10/01-trimstart-trimend.js
@@ -0,0 +1,3 @@
+const hello = '         Hello Wordl!    ';
+console.log(hello.trimStart());//elimina espacios del inicio
+console.log(hello.trimEnd());//elimina espacios del final
---
+++ es10/03-fromentries.js
@@ -0,0 +1,3 @@
+const entries = new Map([['name','Oscar'],['Agre',32]]);
+console.log(entries);
+console.log(Object.fromEntries(entries));
